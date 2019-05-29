ff96009796bdc4add8035a6a0a519c56ce456bc0
Jean-Marc Rakotoarisoa <jmrakotoarisoa-ext@lacoste.com>
2019-05-29 17:59:47 +0200

    implement update and filtering list features

--- src/features/list/list.js
+++ src/features/list/list.js
@@ -5,39 +5,60 @@
  * Handle list view
  *
  */
-import {View} from '../view.js';
+import {dom, FeatureView, publish, subscribe} from '../feature.js';
 
 let instance = null;
 
-class ListViewFeature extends View {
+export class ListViewFeature extends FeatureView {
 
-    static getInstance() {
-        return instance;
+    constructor(_$container) {
+        super(_$container);
+
+        this.todoList = [];
     }
 
-    add(todo) {
+    static start() {
+        if (!instance) {
+            instance = new ListViewFeature(dom('body'));
 
+            subscribe(
+                {'add': instance.add.bind(instance)},
+                {'filter': instance.filter.bind(instance)}
+            );
+
+            instance.render()
+        }
     }
 
-    remove(todo) {
-
+    add({detail: message}) {
+        this.todoList.push({todo: message, isDone: false, isFiltered: false});
+        this.render();
     }
 
-    handle() {
+    filter({detail: filteredTodo}) {
+        if (filteredTodo.length) {
+            this.todoList.forEach((todo) => todo.isFiltered = filteredTodo.includes(todo));
+            this.render();
+        }
+    }
 
+    remove(todoId) {
+        this.todoList.splice(todoId, 1);
+        this.render();
+    }
+
+    render() {
+        console.log('Should render todo list here');
+        // render the list with ability to
+        // - mark a message as done using this.setDone()
+        // - remove a message using this.remove()
+        // - ignore message when it has isFiltered = true
+        // - stroke message and check associated checkbox when it has isDone = true
+        publish({name: 'list', detail: this.todoList});
+    }
+
+    setDone(todoId, isDone) {
+        this.todoList[todoId].isDone = isDone;
+        this.render();
     }
 }
-
-/**
- * Factory that provides a singleton of ListViewFeature
- * @param $container        HTMLElement where the list will be injected
- * @returns ListViewFeature     the instance of ListViewFeature
- */
-export const handleListViewFeature = ($container) => {
-     if (!instance) {
-         instance = new ListViewFeature($container);
-         instance.handle();
-     }
-
-     return instance;
-};
