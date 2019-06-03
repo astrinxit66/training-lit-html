9b187709baab5f1d8c2c7249bf08adb475c92791
Jean-Marc Rakotoarisoa <jmrakotoarisoa-ext@lacoste.com>
2019-06-03 19:51:20 +0200

    adjust style

--- src/features/add/add.tpl.js
+++ src/features/add/add.tpl.js
@@ -18,15 +18,17 @@ const headerStyle = html`
         }
         .todo-add-view .form {
             display: flex;
-            border-radius: 0 10px 10px 0;
-            border: 1px solid rgb(204, 204, 204);
+            border-radius: 10px 10px 0 0;
+            border: 1px solid var(--borderColor);
+            background-color: white;
             overflow: hidden;
+            box-shadow: inset 0 -2px 100px rgba(0, 0, 0, 0.03);
         }
         .todo-add-view input {
             padding: 16px 16px 16px 60px;
             border: none;
-            background-color: rgb(255, 255, 255);
-            box-shadow: inset 0 -2px 1px rgba(0, 0, 0, 0.03);
+            background-color: white;
+            box-shadow: inset 0 -2px 10px rgba(0, 0, 0, 0.03);
             flex: 80 80 auto;
         }
         .todo-add-view input::placeholder {
@@ -40,7 +42,9 @@ const headerStyle = html`
         }
         .todo-add-view button {
             flex: 20 20 100px;
+            margin: 5px;
             border: none;
+            border-radius: 10px;
             background-color: rgb(235, 235, 235);
             font-size: calc(var(--fontSize) * 0.8);
         }
@@ -52,22 +56,28 @@ const headerStyle = html`
     </style>`;
 
 export const addTpl = ({onEnterKeyup, onAddBtnClick}) => {
+  const btnSetEnabled = (isEnabled) => dom('.js-add-btn')[`${isEnabled ? 'remove' : 'set'}Attribute`]('disabled', true);
+
   // the add button is disabled by default. This allow us to handle its state:
   // we enable the add button if there is something into the input.
   const _onKeyup = (e) => {
       const isAddBtnEnabled = e.target.value.trim().length > 0;
 
-      dom('.js-add-btn')[`${isAddBtnEnabled ? 'remove' : 'set'}Attribute`]('disabled', true);
+      btnSetEnabled(isAddBtnEnabled);
       onEnterKeyup(e);
+  };
+
+  const _onBtnClick = (e) => {
+      onAddBtnClick(e);
+      btnSetEnabled(false);
   };
 
   return html`
       ${headerStyle}
       <h1>My awesome todos</h1>
-          <div class="form">
-              <input @keyup="${_onKeyup}" type="text" class="js-add-input" placeholder="What needs to be done?" autofocus>
-              <button @click="${onAddBtnClick}" class="js-add-btn" disabled>Add it!</button>
-          </div>  
-      
+      <div class="form">
+          <input @keyup="${_onKeyup}" type="text" class="js-add-input" placeholder="What needs to be done?" autofocus>
+          <button @click="${_onBtnClick}" class="js-add-btn" disabled>Add it!</button>
+      </div>  
   `;
 };
